2f4a545913fa749baa48ebd93515d1cc87f92062
mazhili <526877521@qq.com>
2022-08-22 14:39:44 +0800

    配置表和http post 初步搞定

--- Assets/Script/Root.cs
+++ Assets/Script/Root.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Threading.Tasks;
 using FairyGUI;
 using Login;
 using UnityEngine;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Script.FGui.Utils;
 
 public class Root : MonoBehaviour
 {
-    public static Newtonsoft.Json.Linq.JObject DeserializeString(string jsonStr)
+    public  Newtonsoft.Json.Linq.JObject DeserializeString(string jsonStr)
     {
         Newtonsoft.Json.Linq.JObject jsonDict = (Newtonsoft.Json.Linq.JObject)JsonConvert.DeserializeObject(jsonStr);
 
@@ -28,14 +28,31 @@ public class Root : MonoBehaviour
         {
             Debug.Log("点击登录按钮" + Application.dataPath);
             my_view.m_des_tip.text = "hhhhhhhhhhhhh";
-            EDItem_Monster monster =EDItem_Monster.GetById(1);
+            EDItem_Monster monster = EDItem_Monster.GetById(1);
             // ItemManager man = Resources.Load<ItemManager>("DataAssets/Item");
             // Item myItem = man.getItemById(10001);
             Debug.Log(monster.attack + "---" + monster.name + "---" + monster.isBoss);
 
             //todo 发送请求，调取后端数据
-            // var usr= JsonConvert.SerializeObject("{name: 'zs', age: 18}");
-            // Debug.Log(usr);
+            string account = "0x4398Dd2508F3ed87b0e36DbA343438523cf6BaAc";
+
+            JObject ob = new JObject();
+            ob["address"] = account;
+
+            string sendData = JsonConvert.SerializeObject(ob);
+            string res_login = await HttpMgr.getInstance().httpPost("api/auth/signIn", sendData);
+            
+            
+            Debug.Log("jsonDict=" + res_login);
+            var jsonDict = DeserializeString(res_login);
+            string sign = jsonDict["data"].Value<string>("msg");
+            string signInfo = jsonDict["data"].Value<string>("msg");
+            Debug.Log("====>"+signInfo+"======>><<"+sign);
+
+            
+            // var message = res_login["data"].Values<string>("msg");
+            // Debug.Log(message);
+            // // Debug.Log(usr);
             // Newtonsoft.Json.Linq.JObject.FromObject(new { Id = 1, Num = 99, Name = "gas" });
             // var result = await HttpMgr.getInstance().httpGet("api/parse/result");
             // Dictionary<string,Object > dic1= JsonUnity.DeserializeStringToDictionary<string,Object>(result);
--- Assets/Script/Utils/HttpMgr.cs
+++ Assets/Script/Utils/HttpMgr.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -15,14 +16,13 @@ namespace Script.FGui.Utils
             {
                 return new HttpMgr();
             }
+
             return HttpMgr._instance;
         }
+
         //发送get请求 特制网络请求
         public async Task<string> httpGet(string url)
         {
-
-            UnityWebRequest unityWebRequest = UnityWebRequest.Get(url);
-            // return "1312313";
             var request = UnityWebRequest.Get(_httpHost + url);
             await request.SendWebRequest();
             // UnityWebRequest.Result result = await request.SendWebRequest();
@@ -35,10 +35,33 @@ namespace Script.FGui.Utils
             {
                 Debug.Log("加载成功");
             }
-            
+
             return request.downloadHandler.text;
         }
-        
-        
+
+        public async Task<string> httpPost(string url, string param)
+        {
+            //todo 字符串转json 
+            var request = new UnityWebRequest(_httpHost + url, "POST");
+
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(param);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw); //添加数据
+            request.downloadHandler = new DownloadHandlerBuffer(); // 数据Buffer
+            request.SetRequestHeader("Content-Type", "application/json"); // 请求头设置
+            await request.SendWebRequest();
+            Debug.Log("Status Code: " + request.result);
+            // UnityWebRequest.Result result = await request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log("error" + request.error);
+                return request.error;
+            }
+            else
+            {
+                 Debug.Log("加载成功" + request.downloadHandler.text);
+            }
+
+            return request.downloadHandler.text;
+        }
     }
 }
